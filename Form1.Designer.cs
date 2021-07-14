e2719a6433accd4086d25a81aeb03821513f1f87
Erica Slotkin <77819164+ferris4936@users.noreply.github.com>
2021-07-13 18:50:22 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,355 @@
+﻿
+namespace SlotkinStudentFormDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.studentIDLabel = new System.Windows.Forms.Label();
+            this.firstNameLabel = new System.Windows.Forms.Label();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.studentIDTextBox = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.coursesLabel = new System.Windows.Forms.Label();
+            this.cis101CheckBox = new System.Windows.Forms.CheckBox();
+            this.cis102CheckBox = new System.Windows.Forms.CheckBox();
+            this.cis103CheckBox = new System.Windows.Forms.CheckBox();
+            this.cis104CheckBox = new System.Windows.Forms.CheckBox();
+            this.scoresLabel = new System.Windows.Forms.Label();
+            this.score101TextBox = new System.Windows.Forms.TextBox();
+            this.score102TextBox = new System.Windows.Forms.TextBox();
+            this.score103TextBox = new System.Windows.Forms.TextBox();
+            this.score104TextBox = new System.Windows.Forms.TextBox();
+            this.majorGroupBox = new System.Windows.Forms.GroupBox();
+            this.ITRadioButton = new System.Windows.Forms.RadioButton();
+            this.webDesignRadioButton = new System.Windows.Forms.RadioButton();
+            this.programmingRadioButton = new System.Windows.Forms.RadioButton();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.summaryLabel = new System.Windows.Forms.Label();
+            this.summaryTextBox = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.majorGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // studentIDLabel
+            // 
+            this.studentIDLabel.AutoSize = true;
+            this.studentIDLabel.Location = new System.Drawing.Point(37, 39);
+            this.studentIDLabel.Name = "studentIDLabel";
+            this.studentIDLabel.Size = new System.Drawing.Size(79, 20);
+            this.studentIDLabel.TabIndex = 0;
+            this.studentIDLabel.Text = "Student ID";
+            // 
+            // firstNameLabel
+            // 
+            this.firstNameLabel.AutoSize = true;
+            this.firstNameLabel.Location = new System.Drawing.Point(37, 86);
+            this.firstNameLabel.Name = "firstNameLabel";
+            this.firstNameLabel.Size = new System.Drawing.Size(80, 20);
+            this.firstNameLabel.TabIndex = 1;
+            this.firstNameLabel.Text = "First Name";
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(37, 133);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(79, 20);
+            this.lastNameLabel.TabIndex = 2;
+            this.lastNameLabel.Text = "Last Name";
+            // 
+            // studentIDTextBox
+            // 
+            this.studentIDTextBox.Location = new System.Drawing.Point(141, 32);
+            this.studentIDTextBox.Name = "studentIDTextBox";
+            this.studentIDTextBox.Size = new System.Drawing.Size(466, 27);
+            this.studentIDTextBox.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(141, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(466, 27);
+            this.textBox1.TabIndex = 4;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(141, 134);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(466, 27);
+            this.lastNameTextBox.TabIndex = 5;
+            // 
+            // coursesLabel
+            // 
+            this.coursesLabel.AutoSize = true;
+            this.coursesLabel.Location = new System.Drawing.Point(57, 207);
+            this.coursesLabel.Name = "coursesLabel";
+            this.coursesLabel.Size = new System.Drawing.Size(60, 20);
+            this.coursesLabel.TabIndex = 6;
+            this.coursesLabel.Text = "Courses";
+            // 
+            // cis101CheckBox
+            // 
+            this.cis101CheckBox.AutoSize = true;
+            this.cis101CheckBox.Location = new System.Drawing.Point(57, 238);
+            this.cis101CheckBox.Name = "cis101CheckBox";
+            this.cis101CheckBox.Size = new System.Drawing.Size(80, 24);
+            this.cis101CheckBox.TabIndex = 7;
+            this.cis101CheckBox.Text = "CIS 101";
+            this.cis101CheckBox.UseVisualStyleBackColor = true;
+            // 
+            // cis102CheckBox
+            // 
+            this.cis102CheckBox.AutoSize = true;
+            this.cis102CheckBox.Location = new System.Drawing.Point(57, 275);
+            this.cis102CheckBox.Name = "cis102CheckBox";
+            this.cis102CheckBox.Size = new System.Drawing.Size(80, 24);
+            this.cis102CheckBox.TabIndex = 8;
+            this.cis102CheckBox.Text = "CIS 102";
+            this.cis102CheckBox.UseVisualStyleBackColor = true;
+            // 
+            // cis103CheckBox
+            // 
+            this.cis103CheckBox.AutoSize = true;
+            this.cis103CheckBox.Location = new System.Drawing.Point(57, 312);
+            this.cis103CheckBox.Name = "cis103CheckBox";
+            this.cis103CheckBox.Size = new System.Drawing.Size(80, 24);
+            this.cis103CheckBox.TabIndex = 9;
+            this.cis103CheckBox.Text = "CIS 103";
+            this.cis103CheckBox.UseVisualStyleBackColor = true;
+            // 
+            // cis104CheckBox
+            // 
+            this.cis104CheckBox.AutoSize = true;
+            this.cis104CheckBox.Location = new System.Drawing.Point(57, 349);
+            this.cis104CheckBox.Name = "cis104CheckBox";
+            this.cis104CheckBox.Size = new System.Drawing.Size(80, 24);
+            this.cis104CheckBox.TabIndex = 10;
+            this.cis104CheckBox.Text = "CIS 104";
+            this.cis104CheckBox.UseVisualStyleBackColor = true;
+            // 
+            // scoresLabel
+            // 
+            this.scoresLabel.AutoSize = true;
+            this.scoresLabel.Location = new System.Drawing.Point(162, 207);
+            this.scoresLabel.Name = "scoresLabel";
+            this.scoresLabel.Size = new System.Drawing.Size(52, 20);
+            this.scoresLabel.TabIndex = 11;
+            this.scoresLabel.Text = "Scores";
+            // 
+            // score101TextBox
+            // 
+            this.score101TextBox.Location = new System.Drawing.Point(162, 237);
+            this.score101TextBox.Name = "score101TextBox";
+            this.score101TextBox.Size = new System.Drawing.Size(125, 27);
+            this.score101TextBox.TabIndex = 12;
+            // 
+            // score102TextBox
+            // 
+            this.score102TextBox.Location = new System.Drawing.Point(162, 274);
+            this.score102TextBox.Name = "score102TextBox";
+            this.score102TextBox.Size = new System.Drawing.Size(125, 27);
+            this.score102TextBox.TabIndex = 13;
+            // 
+            // score103TextBox
+            // 
+            this.score103TextBox.Location = new System.Drawing.Point(162, 311);
+            this.score103TextBox.Name = "score103TextBox";
+            this.score103TextBox.Size = new System.Drawing.Size(125, 27);
+            this.score103TextBox.TabIndex = 14;
+            // 
+            // score104TextBox
+            // 
+            this.score104TextBox.Location = new System.Drawing.Point(162, 348);
+            this.score104TextBox.Name = "score104TextBox";
+            this.score104TextBox.Size = new System.Drawing.Size(125, 27);
+            this.score104TextBox.TabIndex = 15;
+            // 
+            // majorGroupBox
+            // 
+            this.majorGroupBox.Controls.Add(this.ITRadioButton);
+            this.majorGroupBox.Controls.Add(this.webDesignRadioButton);
+            this.majorGroupBox.Controls.Add(this.programmingRadioButton);
+            this.majorGroupBox.Location = new System.Drawing.Point(357, 213);
+            this.majorGroupBox.Name = "majorGroupBox";
+            this.majorGroupBox.Size = new System.Drawing.Size(250, 125);
+            this.majorGroupBox.TabIndex = 16;
+            this.majorGroupBox.TabStop = false;
+            this.majorGroupBox.Text = "Major";
+            // 
+            // ITRadioButton
+            // 
+            this.ITRadioButton.AutoSize = true;
+            this.ITRadioButton.Location = new System.Drawing.Point(7, 89);
+            this.ITRadioButton.Name = "ITRadioButton";
+            this.ITRadioButton.Size = new System.Drawing.Size(42, 24);
+            this.ITRadioButton.TabIndex = 2;
+            this.ITRadioButton.TabStop = true;
+            this.ITRadioButton.Text = "IT";
+            this.ITRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // webDesignRadioButton
+            // 
+            this.webDesignRadioButton.AutoSize = true;
+            this.webDesignRadioButton.Location = new System.Drawing.Point(7, 58);
+            this.webDesignRadioButton.Name = "webDesignRadioButton";
+            this.webDesignRadioButton.Size = new System.Drawing.Size(110, 24);
+            this.webDesignRadioButton.TabIndex = 1;
+            this.webDesignRadioButton.TabStop = true;
+            this.webDesignRadioButton.Text = "Web Design";
+            this.webDesignRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // programmingRadioButton
+            // 
+            this.programmingRadioButton.AutoSize = true;
+            this.programmingRadioButton.Location = new System.Drawing.Point(7, 27);
+            this.programmingRadioButton.Name = "programmingRadioButton";
+            this.programmingRadioButton.Size = new System.Drawing.Size(121, 24);
+            this.programmingRadioButton.TabIndex = 0;
+            this.programmingRadioButton.TabStop = true;
+            this.programmingRadioButton.Text = "Programming";
+            this.programmingRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(492, 349);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(115, 38);
+            this.submitButton.TabIndex = 17;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // summaryLabel
+            // 
+            this.summaryLabel.AutoSize = true;
+            this.summaryLabel.Location = new System.Drawing.Point(57, 395);
+            this.summaryLabel.Name = "summaryLabel";
+            this.summaryLabel.Size = new System.Drawing.Size(71, 20);
+            this.summaryLabel.TabIndex = 18;
+            this.summaryLabel.Text = "Summary";
+            // 
+            // summaryTextBox
+            // 
+            this.summaryTextBox.Location = new System.Drawing.Point(57, 433);
+            this.summaryTextBox.Multiline = true;
+            this.summaryTextBox.Name = "summaryTextBox";
+            this.summaryTextBox.Size = new System.Drawing.Size(550, 142);
+            this.summaryTextBox.TabIndex = 19;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(696, 555);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(149, 20);
+            this.linkLabel1.TabIndex = 20;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Cengage Home Page";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SlotkinStudentFormDemo.Properties.Resources.IMG_0260_14773_;
+            this.pictureBox1.Location = new System.Drawing.Point(696, 384);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(197, 168);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(916, 621);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.summaryTextBox);
+            this.Controls.Add(this.summaryLabel);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.majorGroupBox);
+            this.Controls.Add(this.score104TextBox);
+            this.Controls.Add(this.score103TextBox);
+            this.Controls.Add(this.score102TextBox);
+            this.Controls.Add(this.score101TextBox);
+            this.Controls.Add(this.scoresLabel);
+            this.Controls.Add(this.cis104CheckBox);
+            this.Controls.Add(this.cis103CheckBox);
+            this.Controls.Add(this.cis102CheckBox);
+            this.Controls.Add(this.cis101CheckBox);
+            this.Controls.Add(this.coursesLabel);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.studentIDTextBox);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.firstNameLabel);
+            this.Controls.Add(this.studentIDLabel);
+            this.Name = "Form1";
+            this.Text = "Student Form";
+            this.majorGroupBox.ResumeLayout(false);
+            this.majorGroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label studentIDLabel;
+        private System.Windows.Forms.Label firstNameLabel;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.TextBox studentIDTextBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.Label coursesLabel;
+        private System.Windows.Forms.CheckBox cis101CheckBox;
+        private System.Windows.Forms.CheckBox cis102CheckBox;
+        private System.Windows.Forms.CheckBox cis103CheckBox;
+        private System.Windows.Forms.CheckBox cis104CheckBox;
+        private System.Windows.Forms.Label scoresLabel;
+        private System.Windows.Forms.TextBox score101TextBox;
+        private System.Windows.Forms.TextBox score102TextBox;
+        private System.Windows.Forms.TextBox score103TextBox;
+        private System.Windows.Forms.TextBox score104TextBox;
+        private System.Windows.Forms.GroupBox majorGroupBox;
+        private System.Windows.Forms.RadioButton ITRadioButton;
+        private System.Windows.Forms.RadioButton webDesignRadioButton;
+        private System.Windows.Forms.RadioButton programmingRadioButton;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.Label summaryLabel;
+        private System.Windows.Forms.TextBox summaryTextBox;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,201 @@
+﻿using StudentForm;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;   
+
+namespace SlotkinStudentFormDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            string major = "TBD";
+            if (programmingRadioButton.Checked)
+            {
+                major = programmingRadioButton.Text;
+            }
+            else if (webDesignRadioButton.Checked)
+            {
+                major = webDesignRadioButton.Text;
+            }
+            else if (ITRadioButton.Checked)
+            {
+                major = ITRadioButton.Text;
+            }
+
+            //Instantiate a student
+            Student st = new Student(studentIDTextBox.Text,
+                                     textBox1.Text,  
+                                     lastNameTextBox.Text,
+                                     major);
+            st.StudentNumber = studentIDTextBox.Text;
+
+            //Count number of check boxes checked
+            int count = 0;
+            if (cis101CheckBox.Checked)
+            {
+                count++;
+            }
+            if (cis102CheckBox.Checked)
+            {
+                count++;
+            }
+            if (cis103CheckBox.Checked)
+            {
+                count++;
+            }
+            if (cis104CheckBox.Checked)
+            {
+                count++;
+            }
+
+            string[] courses = new string[count];
+            int[] scores = new int[count];
+
+            int index = 0;
+            if (cis101CheckBox.Checked)
+            {
+                courses[index] = cis101CheckBox.Text;
+                scores[index++] = Convert.ToInt32(score101TextBox.Text);
+            }
+            if (cis102CheckBox.Checked)
+            {
+                courses[index] = cis102CheckBox.Text;
+                scores[index++] = Convert.ToInt32(score102TextBox.Text);
+            }
+            if (cis103CheckBox.Checked)
+            {
+                courses[index] = cis103CheckBox.Text;
+                scores[index++] = Convert.ToInt32(score103TextBox.Text);
+            }
+            if (cis104CheckBox.Checked)
+            {
+                courses[index] = cis104CheckBox.Text;
+                scores[index++] = Convert.ToInt32(score104TextBox.Text);
+            }
+
+            //Set new arrays into student object
+            st.Scores = scores;
+            st.Courses = courses;
+
+            //display output
+
+            string summary =
+                "Student's Major is " + st.Major + "."+
+                "\r\nStudent's First Name is " + st.FirstName +"." +
+                "\r\nStudent's Last Name is " + st.LastName + "." +
+                "\r\nStudent's Average is " + st.Average + "." +
+                "\r\nCourses: ";
+            foreach (string course in st.Courses)
+            {
+                summary += "\r\n" + course;            
+            }
+            summaryTextBox.Text = summary;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("IExplore", "http://www.couorse.com");
+            linkLabel1.LinkVisited = true;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            string major = "TBD";
+            if (programmingRadioButton.Checked)
+            {
+                major = programmingRadioButton.Text;
+            }
+            else if (webDesignRadioButton.Checked)
+            {
+                major = webDesignRadioButton.Text;
+            }
+            else if (ITRadioButton.Checked)
+            {
+                major = ITRadioButton.Text;
+            }
+
+            //Instantiate a student
+            Student st = new Student(studentIDTextBox.Text,
+                                     textBox1.Text,
+                                     lastNameTextBox.Text,
+                                     major);
+            st.StudentNumber = studentIDTextBox.Text;
+
+            //Count number of check boxes checked
+            int count = 0;
+            if (cis101CheckBox.Checked)
+            {
+                count++;
+            }
+            if (cis102CheckBox.Checked)
+            {
+                count++;
+            }
+            if (cis103CheckBox.Checked)
+            {
+                count++;
+            }
+            if (cis104CheckBox.Checked)
+            {
+                count++;
+            }
+
+            string[] courses = new string[count];
+            int[] scores = new int[count];
+
+            int index = 0;
+            if (cis101CheckBox.Checked)
+            {
+                courses[index] = cis101CheckBox.Text;
+                scores[index++] = Convert.ToInt32(score101TextBox.Text);
+            }
+            if (cis102CheckBox.Checked)
+            {
+                courses[index] = cis102CheckBox.Text;
+                scores[index++] = Convert.ToInt32(score102TextBox.Text);
+            }
+            if (cis103CheckBox.Checked)
+            {
+                courses[index] = cis103CheckBox.Text;
+                scores[index++] = Convert.ToInt32(score103TextBox.Text);
+            }
+            if (cis104CheckBox.Checked)
+            {
+                courses[index] = cis104CheckBox.Text;
+                scores[index++] = Convert.ToInt32(score104TextBox.Text);
+            }
+
+            //Set new arrays into student object
+            st.Scores = scores;
+            st.Courses = courses;
+
+            //display output
+
+            string summary =
+                "Student's Major is " + st.Major + "." +
+                "\r\nStudent's First Name is " + st.FirstName + "." +
+                "\r\nStudent's Last Name is " + st.LastName + "." +
+                "\r\nStudent's Average is " + st.Average + "." +
+                "\r\nCourses: ";
+            foreach (string course in st.Courses)
+            {
+                summary += "\r\n" + course;
+            }
+            summaryTextBox.Text = summary;
+        }  
+    }
+}
+
